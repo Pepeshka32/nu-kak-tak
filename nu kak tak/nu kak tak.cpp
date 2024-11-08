52d7c3890c8922320a1aba3ec0a5d839850a3a2f
Pepega32 <iwashkalolesnikow@gmail.com>
2024-11-08 19:40:08 +0200

    Add project files.

---
+++ nu kak tak/nu kak tak.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+public:
+    static int count;
+    Point(int x, int y) : x(x), y(y) {
+        count++;
+    }
+    static int getCount() {
+        return count;
+    }
+
+private:
+    int x, y;
+};
+
+int Point::count = 0;
+
+int main() {
+    Point p1(1, 2);
+    Point p2(3, 4);
+    Point p3(5, 6);
+
+    cout << "Point: " << Point::getCount() << std::endl;
+
+    return 0;
+}
